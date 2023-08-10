48a448fbc3d04d5816dd7eaaf07bee662338415a
NeilB1ju <neilbijun@gmail.com>
2023-08-11 02:09:19 +0530

    Added Add Task button

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import tabsListeners from "./tabs";
 tabsListeners();
 
 
-//Added the item container class to all tabs. 
 //Now work on generating an item and styling it 
-//Then work on the button to add an item.
 //Style the tasks 
 //Add the projects section
--- src/tabs.js
+++ src/tabs.js
@@ -1,26 +1,28 @@
+import plusIcon from './images/plus.svg'
+
 const main = document.querySelector('.main');
 const today = document.querySelector(".today-button");
 const inbox = document.querySelector(".inbox-button");
 const thisWeek = document.querySelector(".this-week-button");
 
 //Creating the dom elements to be displayed in the various tabs
+const inboxDom = document.createElement('div');
 const todayDom = document.createElement('div');
 const thisWeekDom = document.createElement('div');
-const inboxDom = document.createElement('div');
-
-todayDom.innerHTML += `
-    <h2>Today</h2>
-    <div class="item-container">
-`
-
-thisWeekDom.innerHTML += `
-    <h2>This Week</h2>
-    <div class="item-container">
-`
 
 inboxDom.innerHTML += `
     <h2>Inbox</h2>
-    <div class="item-container">
+    <div class="item-container"></div>
+`
+createAddTask(inboxDom);
+
+todayDom.innerHTML += `
+    <h2>Today</h2>
+    <div class="item-container"></div>
+`
+thisWeekDom.innerHTML += `
+    <h2>This Week</h2>
+    <div class="item-container"></div>
 `
 
 
@@ -62,3 +64,39 @@ function changeActive () {
 }
 
 
+//Function to add a create task button to any tab when necessary
+function createAddTask(domElement) {
+    const addTaskButton = document.createElement('button');
+    addTaskButton.classList.add('add-task');
+    const plusImg = document.createElement('img');
+    plusImg.src = plusIcon;
+    const addTaskText = document.createElement('p');
+    addTaskText.innerHTML = "Add Task";
+    addTaskButton.appendChild(plusImg);
+    addTaskButton.appendChild(addTaskText);
+    domElement.appendChild(addTaskButton);
+
+    //Functionality of the add task button (Add task button dissapears and the input appears)
+    addTaskButton.addEventListener('click' , () => {
+        const inputContainer = document.createElement('div');
+        inputContainer.classList.add('input-container');
+        const input = document.createElement('input');
+        input.type = "Text";
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('button-container');
+        const addButton = document.createElement('button');
+        addButton.classList.add('add-button');
+        addButton.innerHTML = "Add";
+        const cancelButton = document.createElement('button');
+        cancelButton.classList.add('cancel-button');
+        cancelButton.innerHTML = "Cancel";
+        
+        buttonContainer.appendChild(addButton);
+        buttonContainer.appendChild(cancelButton);
+        inputContainer.appendChild(input);
+        inputContainer.appendChild(buttonContainer);
+
+        addTaskButton.style.display = 'none';
+        domElement.appendChild(inputContainer);
+    });
+}
