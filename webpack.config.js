280079785533924f36e68fe4e809420ad7a1b36c
NeilB1ju <neilbijun@gmail.com>
2023-08-16 03:53:27 +0530

    added dist folder to repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode : 'development',
+  mode : 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
   },
   module: {
     rules: [
